9db0f53fda267433615b049cc3fd358890997edf
mekdamahmed <mekdammurad2006@gmail.com>
2025-07-25 21:53:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,3 +48,18 @@ if __name__ == "__main__":
     else:
         bot = MyBot()
         bot.run(TOKEN)
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
